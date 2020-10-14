da7ad0b78117b54e0b749348f6561e47ed9eb581
Alex Eesley <alexandra.eesley@gmail.com>
2020-10-14 14:42:17 -0500

    added some information from TA Paul

---
+++ Develop/index.js
@@ -0,0 +1,25 @@
+
+
+// PAUL TA May want to import const generateMarkdown = require('./utils/generateMarkdown');
+
+// array of questions for user
+const questions = [
+
+];
+
+
+// function to write README file
+function writeToFile(fileName, data) {
+    // PAUL TA generateMarkdown(data);
+}
+
+// function to initialize program
+function init() {
+    // PAUL TA ask the inquirer stuff
+    // one question about file name...
+    // writeToFile(saidFileName, dataFromQuestions)
+
+}
+
+// function call to initialize program
+init();
