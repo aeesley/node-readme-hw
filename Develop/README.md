cbc2a58451fe61bcf4446de10ab8fd07c183152e
Alex Eesley <alexandra.eesley@gmail.com>
2020-10-14 21:05:43 -0500

    reformatting how data output comes on the readme page so it looks a little bit cleaner

--- Develop/README.md
+++ Develop/README.md
@@ -3,3 +3,43 @@
 
 # Project TItle: asdf
 
+# Project TItle: asdf
+
+# Project Title: test
+          # Project Description: test
+          # Installation Instructions: tes
+          # Usage Instructions: tes
+          # Contribution Guidelines: tes
+          # License: tes
+          # Sources: test
+          # Author(s): undefined
+          # Github Username: test
+          # Github Repo Link: tes
+# Project Title: test
+
+    # Project Description: 
+    slkersleis
+
+    # Installation Instructions: 
+    sleisfleise
+
+    # Usage Instructions: 
+    sliefsleise
+
+    # Contribution Guidelines: 
+    lisenfslies
+
+    # License: 
+    slienfslies
+
+    # Sources: 
+    lsienfsliens
+
+    # Author: 
+    undefined
+
+    # Github Username: 
+    lsiefnlsie
+
+    # Github Repo Link: 
+    slefnseis
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,15 +1,42 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# Project Title: ${data.title}
-          # Project Description: ${data.description}
-          # Installation Instructions: ${data.installation}
-          # Usage Instructions: ${data.usage}
-          # Contribution Guidelines: ${data.contribution}
-          # License: ${data.license}
-          # Sources: ${data.sources}
-          # Author(s): ${data.author}
-          # Github Username: ${data.username}
-          # Github Repo Link: ${data.repo}
+
+  # Project Description: 
+  
+  ${data.description}
+
+  # Installation Instructions: 
+  
+  ${data.installation}
+
+  # Usage Instructions: 
+  
+  ${data.usage}
+
+  # Contribution Guidelines: 
+  
+  ${data.contribution}
+
+  # License: 
+  
+  ${data.license}
+
+  # Sources: 
+  
+  ${data.sources}
+
+  # Author: 
+  
+  ${data.author}
+
+  # Github Username: 
+  
+  ${data.username}
+
+  # Github Repo Link: 
+  
+  ${data.repo}
 `;
 }
 
